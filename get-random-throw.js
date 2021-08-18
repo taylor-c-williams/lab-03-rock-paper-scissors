985ea27ed694d0bbb56adec0b9a453bcd775f5e7
taylor-c-williams <taylor.callan.williams@gmail.com>
2021-08-18 16:30:33 -0700

    game works

--- get-random-throw.js
+++ get-random-throw.js
@@ -5,10 +5,24 @@ export function getRandomThrow() {
 }
 
 export function didUserWin(player, computer) {
-    if (player === computer) {
+    if (player === 'rock' && computer === 'rock') {
         return 'draw';
-    } else if (player !== computer) {
-        return 'nope';
+    } else if (player === 'rock' && computer === 'paper') {
+        return 'computer';
+    } else if (player === 'rock' && computer === 'scissors') {
+        return 'player';
+    } else if (player === 'paper' && computer === 'rock') {
+        return 'player';
+    } else if (player === 'paper' &&  computer === 'paper') {
+        return 'draw';
+    } else if (player === 'paper' && computer === 'scissors') {
+        return 'computer';
+    } else if (player === 'scissors' && computer === 'rock') {
+        return 'computer';
+    } else if (player === 'scissors' &&  computer === 'paper') {
+        return 'player';
+    } else if (player === 'scissors' && computer === 'scissors') {
+        return 'draw';
     }
 }
 
